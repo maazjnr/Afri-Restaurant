c4e53e53b2745da32a7412236e857e57f83f13f1
Maaz Junior <goalmaaz@gmail.com>
2022-08-10 16:11:11 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
 
-      <h1>Afri Restaurant</h1>
-
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
