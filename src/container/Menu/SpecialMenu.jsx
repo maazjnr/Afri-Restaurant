4d665470e48a84c1eb38646677441ca449f62ceb
Maaz Junior <goalmaaz@gmail.com>
2022-08-12 23:40:00 +0100

    working on menuitem section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -12,17 +12,35 @@ const SpecialMenu = () => (
       <h1 className='headtext__cormorants'>Today's Special</h1>
     </div>
 
-    <div className='app__specialMenu-Menu'>
+    <div className='app__specialMenu-menu'>
       <div className='app__specialMenu-menu_wine flex__center' >
         <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
         <div className='app__specialMenu_menu_items'>
           {data.wines.map((wine, beer) => (
-            <p>{wine.title}</p>
+            <MenuItem key={wine.title} title={wine.title} 
+            price={wine.price} tags={wine.tags}/>
+          ))}
+        </div>
+      </div>
+      
+      <div className='app__specialMenu-menu_img'>
+      <img src={images.menu} alt="menu-image" />
+    </div>
+
+    <div className='app__specialMenu-menu_cocktails flex__center' >
+        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <div className='app__specialMenu_menu_items'>
+          {data.cocktails.map((cocktail, beer) => (
+            <MenuItem key={cocktail.title} title={cocktail.title} 
+            price={cocktail.price} tags={cocktail.tags}/>
           ))}
         </div>
       </div>
     </div>
 
+    <div style={{marginTop: '15px'}}>
+      <button type='button' className='custom__button'>View More</button>
+    </div>
   </div>
 );
 
