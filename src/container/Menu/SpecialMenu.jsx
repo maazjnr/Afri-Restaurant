65e5fd8bc1d4b4c09d83c3bea63cbd054d8f0e9e
Maaz Junior <goalmaaz@gmail.com>
2022-08-16 23:31:40 +0100

    finised laurels section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './SpecialMenu.css';
 
 import { SubHeading, MenuItem } from '../../components';
-
 import { images, data } from '../../constants';
 
 const SpecialMenu = () => (
