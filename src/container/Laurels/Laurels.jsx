14870c41b74f419bc39061a430925ecee5fadd89
Maaz Junior <goalmaaz@gmail.com>
2022-08-16 21:45:53 +0100

    Laurels Page

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import './Laurels.css';
 
 const Laurels = () => (
@@ -8,4 +9,4 @@ const Laurels = () => (
   </div>
 );
 
-export default Laurels;
+export default Laurels
