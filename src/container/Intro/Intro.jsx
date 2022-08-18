b60c5b7f08b4f6c1599e992eedd11befe21f8395
Maaz Junior <goalmaaz@gmail.com>
2022-08-18 17:04:24 +0100

    fixing bugs

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,20 +1,57 @@
-import React from "react";
-import { meal } from "../../constants";
+import React from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
-import "./Intro.css";
+import { meal } from '../../constants';
+import './Intro.css';
 
 const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+
   return (
     <div className="app__video">
       <video
+        ref={vidRef}
         src={meal}
-        width="320"
-        height="240"
         type="video/mp4"
-        controls
-      ></video>
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef.current.pause();
+            } else {
+              vidRef.current.play();
+            }
+          }}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Intro;
+
+
+// return (
+//   <div className="app__video">
+//     <video
+//       src={meal}
+//       width="320"
+//       height="240"
+//       type="video/mp4"
+//       controls
+//     ></video>
+//   </div>
+// );
